9d6d07d8e777c87e41728ff2228471840ad2bff8
Gaston Funes <gtfunes@gmail.com>
2019-12-21 03:18:57 -0300

    Better install script, switched to hassio

--- provision.sh
+++ provision.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 apt-get update
-apt-get install git docker docker-compose -y
-systemctl enable docker && systemctl start docker
-git clone https://github.com/gtfunes/vagrant-homeassistant.git
-chown -R vagrant:vagrant vagrant-homeassistant
-cd vagrant-homeassistant
-cp .env.example .env
-usermod -aG docker $(whoami)
-docker-compose up -d
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get install docker-ce jq curl avahi-daemon libavahi-compat-libdnssd-dev dbus apparmor-utils network-manager -y
+curl -sL https://raw.githubusercontent.com/home-assistant/hassio-installer/master/hassio_install.sh | bash -s
