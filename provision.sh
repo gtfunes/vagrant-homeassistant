4d2ad05f7c1bbeb3cb777d87bff8371ccaac764e
Gaston Funes <gtfunes@gmail.com>
2019-12-23 23:56:18 -0300

    Add ffmpeg package for camera streaming support

--- provision.sh
+++ provision.sh
@@ -4,5 +4,5 @@ apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get install docker-ce jq curl avahi-daemon libavahi-compat-libdnssd-dev dbus apparmor-utils network-manager -y
+apt-get install docker-ce jq curl ffmpeg avahi-daemon libavahi-compat-libdnssd-dev dbus apparmor-utils network-manager -y
 curl -sL https://raw.githubusercontent.com/home-assistant/hassio-installer/master/hassio_install.sh | bash -s
